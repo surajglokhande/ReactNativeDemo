e87fa34201be59c3df2cd5764cf2220ae2f94176
surajglokhande <surajglokhande@gmail.com>
2025-08-06 16:12:44 +0530

    Refactor FlatMap component to use SafeAreaView, improve layout, and update FlatList styling; add styles for title and container

--- index.js
+++ index.js
@@ -5,9 +5,10 @@
 import { AppRegistry } from 'react-native';
 // import App from './rn_practice/Hooks/App';
 // import App from './rn_practice/rn_customComponent/CustomComponent'
-import App from "./rn_practice/rn_Modal/Modal";
+// import App from "./rn_practice/rn_Modal/Modal";
 // import App from './App';
 // import App from "./rn_practice/rn_TextInput/App";
+import App from "./rn_practice/rn_FlatMap/FlatMap";
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- rn_practice/rn_FlatMap/FlatMap.jsx
+++ rn_practice/rn_FlatMap/FlatMap.jsx
@@ -1,4 +1,4 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function FlatMapComp() {
 
@@ -26,21 +26,37 @@ export default function FlatMapComp() {
     ]
 
     return (
-        <View>
-            <Text>This is a Flatmap Component</Text>
-            <FlatList
-                style={{ maxHeight: '50%', backgroundColor: 'gray' }}
-                data={list}
-                keyExtractor={item => item.id}
-                renderItem={({ item }) => {
-                    <View style={{ margin: 10 }}>
-                        <Text style={{ backgroundColor: 'red' }}>
-                            {item.name}
-                        </Text>
-                    </View>
-                }}
-            >
-            </FlatList >
-        </View >
+        <SafeAreaView style={styles.superContainer}>
+            <View>
+                <Text style={styles.title}>This is a Flatmap Component</Text>
+                <FlatList
+                    style={{ maxHeight: '20%', backgroundColor: 'yellow' }}
+                    data={list}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => {
+                        return (
+                            <View style={{ alignItems: 'center' }}>
+                                <Text>
+                                    {item.name}
+                                </Text>
+                            </View>
+                        )
+                    }}
+                />
+            </View >
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    title: {
+        textAlign: 'center',
+        margin: 20,
+        backgroundColor: 'gray'
+    },
+    superContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
